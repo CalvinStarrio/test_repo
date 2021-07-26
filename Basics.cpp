372937dbcb55240c40a84f89ab833ee4e06ab55e
CalvinStarrio <67429121+CalvinStarrio@users.noreply.github.com>
2021-07-26 17:05:30 +0800

    first commit

---
+++ Basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+auto function(){
+    
+}
+
+int main(){
+    ofstream out1 ,out2;
+    out1 = out2;
+    ofstream print(ofstream);
+    out2 = print(out2);
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ << resetiosflags(ios::left) 
